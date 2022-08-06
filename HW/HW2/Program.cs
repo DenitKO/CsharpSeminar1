b7b7f4bb1cceb4ae52a41fc68dfbda8519f980c3
DenitKO <motovdenis@mail.ru>
2022-08-07 01:48:53 +0300

    add second programm, need to fix here

---
+++ HW/HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+//int count = 1;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.Write("max = ");
+Console.Write(b);
