fcd74a900012c2452923e774accb723323c182ef
DenitKO <motovdenis@mail.ru>
2022-08-07 19:18:51 +0300

    add programm even_odd HW3

--- HW/HW3/Program.cs
+++ HW/HW3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a % 2) == 0) 
+{
+    Console.Write(a);
+    Console.Write(" = Четное");
+}
+else 
+{
+    Console.Write(a);
+    Console.Write(" = Нечетное");
+}
