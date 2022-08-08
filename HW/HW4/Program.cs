bce1cbcf2ba842d7a21821ae74f0045ae93e661f
DenitKO <motovdenis@mail.ru>
2022-08-08 19:49:49 +0300

    add new programm HW4

--- HW/HW4/Program.cs
+++ HW/HW4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число n");
+int n  = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(", ");
+    }
+}
